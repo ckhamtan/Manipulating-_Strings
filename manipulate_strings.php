c26b2c62ea57cce0ee937fbfa30b4ff00ba54269
ckhamtan <60579464+ckhamtan@users.noreply.github.com>
2020-03-01 12:20:47 -0700

    Add files via upload

---
+++ manipulate_strings.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$string1="Hello Chari Khamtan";
+echo substr($string1,0,11)."<br>";
+
+$string1="Hello Chari Khamtan";
+echo substr_replace("$string1","Hi",0,5)."<br>";
+
+echo strlen("Hello Chari Khamtan")."<br>";
+
+
+?>
+
+
+</body>
+</html>
